533c8e0d31a96ce34ea27dd6103eb45f7d065f4a
Aaron Tam <aarontam@gmail.com>
2016-05-02 01:11:56 -0700

    Utilize token-based access.

--- index.js
+++ index.js
@@ -2,41 +2,34 @@ var Firebase = require('firebase'),
 	bodyParser = require('body-parser'),
 	express = require('express'),
 	app = express(),
-	username = process.argv[2] || '',
-	password = process.argv[3] || '',
-	port = process.argv[4] || 3000,
+	masterToken = process.argv[2] || '',
+	port = process.argv[3] || 3000,
 	endpoint = 'https://enyostandup.firebaseio.com/';
 
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
  
-app.post('/', function (req, res) {
-	console.log(req.body);
-	var name = req.body.user_name,
+app.get('/', function (req, res) {
+	var token = req.body.token,
+		name = req.body.user_name,
 		userId = req.body.user_id,
 		msg = req.body.text,
 		ref;
 
+	if (masterToken !== token) {
+		res.send('Access denied.');
+		return;
+	}
+
 	if (msg) {
-		 ref = new Firebase(endpoint + userId)
-		ref.authWithPassword({
-			email    : username,
-			password : password
-		}, function(error, authData) {
-			if (error) {
-				res.send({
-					'text': 'There was an error saving your update.'
-				});
-			} else {
-				ref.set({
-					name: name,
-					msg: msg,
-					date: Date.now()
-				});
-				res.send();
-			}
+		ref = new Firebase(endpoint + userId);
+		ref.set({
+			name: name,
+			msg: msg,
+			date: Date.now()
 		});
+		res.send();
 
 		// Note: the ephemeral message is sufficient for now
 		// res.send({
@@ -45,37 +38,25 @@ app.post('/', function (req, res) {
 		// });
 	} else {
 		ref = new Firebase(endpoint);
-		ref.authWithPassword({
-			email    : username,
-			password : password
-		}, function(error, authData) {
-			if (error) {
-				res.send({
-					'response_type': 'in_channel',
-					'text': 'There was an error retrieving the standup updates.'
-				});
-			} else {
-				ref.on('value', function (snapshot) {
-					var items = snapshot.val(),
-						text = '',
-						item, key, idx;
+		ref.on('value', function (snapshot) {
+			var items = snapshot.val(),
+				text = '',
+				item, key, idx;
 
-					for (key in items) {
-						item = items[key];
-						text += '**' + item.name + ':** ' + item.msg + '<br />';
-					}
-
-					res.send({
-						'response_type': 'in_channel',
-						'text': text
-					});
-				}, function (errorObject) {
-					res.send({
-						'response_type': 'in_channel',
-						'text': 'The read failed: ' + errorObject.code
-					});
-				});
+			for (key in items) {
+				item = items[key];
+				text += '**' + item.name + ':** ' + item.msg + '<br />';
 			}
+
+			res.send({
+				'response_type': 'in_channel',
+				'text': text
+			});
+		}, function (errorObject) {
+			res.send({
+				'response_type': 'in_channel',
+				'text': 'The read failed: ' + errorObject.code
+			});
 		});
 	}
 });
